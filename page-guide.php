60c6972861863c35488157b6f41ca8ac9ff5dea4
martins <wendigotech@gmail.com>
2020-01-05 23:31:46 +0100

    6

--- page-guide.php
+++ page-guide.php
@@ -3,7 +3,7 @@
 <div class="container px-xl-0 relative text-center text-md-left"> 
     <div class="pb-165 inner pt-230"> 
         <div class="mb-3 logo d-block d-xl-none logo_mobile">
-            <a href="<?php echo esc_url( get_home_url() ); ?>"> <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/imgs/frank-logo.svg" alt="Frankenstein logo" height="40"></a>
+            <a href="<?php echo esc_url( get_home_url() ); ?>"> <img src="<?php echo PG_Image::getUrl( get_theme_mod( 'logo1', esc_url( get_template_directory_uri() . '/imgs/frank-logo.svg' ) ), 'large' ) ?>" alt="Frankenstein logo" height="60"></a>
         </div>                         
         <h1 data-aos-duration="500" data-aos="fade-down" data-aos-delay="0"><?php the_title(); ?></h1> 
         <div class="mt-20 mw-500 color-heading f-18 medium op-7 text-adaptive"> 
