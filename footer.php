f48593761ebc940e1df06127a0fb372d57337db8
martins <wendigotech@gmail.com>
2020-01-05 23:18:57 +0100

    4

--- footer.php
+++ footer.php
@@ -36,7 +36,7 @@
                     <div class="col-lg-6"> 
                         <div class="row"> 
                             <div class="col-xl-4 col-lg-6" data-aos-duration="500" data-aos="fade-down" data-aos-delay="0"> 
-                                <a href="<?php echo esc_url( get_home_url() ); ?>"><img src="<?php echo esc_url( get_template_directory_uri() ); ?>/imgs/frank-logo-white.svg" alt="Frankenstein logo" height="70"> </a> 
+                                <a href="<?php echo esc_url( get_home_url() ); ?>"><img src="<?php echo PG_Image::getUrl( get_theme_mod( 'logo1', esc_url( get_template_directory_uri() . '/imgs/frank-logo-white.svg' ) ), 'large' ) ?>" alt="Frankenstein logo" height="70"> </a> 
                             </div>                             
                             <div class="mt-15 mb-15 my-lg-0 col-xl-8 col-lg-6" data-aos-duration="500" data-aos="fade-down" data-aos-delay="250"> 
                                 <div class="text-adaptive">
